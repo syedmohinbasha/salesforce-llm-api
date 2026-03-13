788b23879d6699b7983ce9a6a0b590499e2a98bf
syed mohin basha <141336904+syedmohinbasha@users.noreply.github.com>
2026-03-13 09:33:48 +0000

    updated API authentication

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 import requests
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI()
 
@@ -13,41 +10,30 @@ class PromptRequest(BaseModel):
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
-
-headers = {
-    "Authorization": f"Bearer {GROQ_API_KEY}",
-    "Content-Type": "application/json"
-}
-
-@app.get("/")
-def home():
-    return {"message": "Groq LLM Middleware API running"}
-
 @app.post("/generate")
-def generate_text(request: PromptRequest):
+def generate_text(request: PromptRequest, authorization: str = Header(None)):
+
+    if authorization != "Bearer salesforce-secret":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    url = "https://api.groq.com/openai/v1/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {GROQ_API_KEY}",
+        "Content-Type": "application/json"
+    }
 
     payload = {
         "model": "llama-3.1-8b-instant",
         "messages": [
-            {
-                "role": "user",
-                "content": request.prompt
-            }
+            {"role": "user", "content": request.prompt}
         ]
     }
 
-    response = requests.post(
-        GROQ_URL,
-        headers=headers,
-        json=payload
-    )
+    response = requests.post(url, headers=headers, json=payload)
 
     data = response.json()
 
-    ai_text = data["choices"][0]["message"]["content"]
-
     return {
-        "prompt": request.prompt,
-        "response": ai_text
+        "response": data["choices"][0]["message"]["content"]
     }
